90cf3a0494da54de60bf4d381907458dbaea4252
Javi.LeL <javier212002@gmail.com>
2024-01-31 17:01:08 +0100

    añadiendo las palabras necesarias y eliminando archivos inecesarios para la fase

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,13 +5,12 @@ import java.util.Arrays;
 import java.util.List;
 
 import Hilos.SaveTools;
-import Hilos.UrlTools;
 
 public class Main {
     final static String raiz = "folder";
     static List<String> palabras;
     public static void main(String[] args) {
-        palabras = new ArrayList<>(Arrays.asList(UrlTools.procesadorPalabras(HtmlPrueba.html)));
+        palabras = new ArrayList<>(Arrays.asList(new String[]{"libro", "casa", "árbol", "perro", "gato", "sol", "luna", "nube", "estrella", "montaña", "río", "mar", "flor", "pájaro", "insecto", "animal", "persona", "mundo"}));
         palabras.stream()
             .map(x->SaveTools.getMD5(x))
             .map(x->SaveTools.generatePath(raiz, x))
