e66f34bbb9daee244b312dd06c888bd51dac0a3c
Javi.LeL <javier212002@gmail.com>
2024-01-31 17:05:43 +0100

    añadiendo los links

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,7 +10,24 @@ public class Main {
     final static String raiz = "folder";
     static List<String> palabras;
     public static void main(String[] args) {
-        palabras = new ArrayList<>(Arrays.asList(new String[]{"libro", "casa", "árbol", "perro", "gato", "sol", "luna", "nube", "estrella", "montaña", "río", "mar", "flor", "pájaro", "insecto", "animal", "persona", "mundo"}));
+        palabras = new ArrayList<>(Arrays.asList(new String[]{
+            "www.elpais.es",
+            "www.abc.es",
+            "www.elmundo.es",
+            "www.20minutos.es",
+            "www.ivoox.com",
+            "www.google.es",
+            "www.meneame.net",
+            "www.as.es",
+            "www.marca.es",
+            "www.elcorteingles.es",
+            "www.ibm.es",
+            "www.oracle.es",
+            "www.microsoft.es",
+            "www.valladolid.es",
+            "www.fmdva.org"
+
+        }));
         palabras.stream()
             .map(x->SaveTools.getMD5(x))
             .map(x->SaveTools.generatePath(raiz, x))
